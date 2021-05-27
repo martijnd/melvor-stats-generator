e61ad807df82f5d170df94749c53ee3b0a3b5316
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-05-27 22:43:00 +0200

    Save character name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import axios from "axios";
 import Layout from "../components/Layout";
 
@@ -10,25 +10,38 @@ const IndexPage = () => {
 
   async function onSubmit(e: FormEvent) {
     e.preventDefault();
+    localStorage.name = name;
     try {
-      setError('');
+      setError("");
       const data = await axios.post("/api/image", {
         data: saveData,
         name,
       });
-      
+
       setImage(data.data);
     } catch (e) {
-      setError('Something went wrong. Is the save file correct?');
+      setError("Something went wrong. Is the save file correct?");
     }
   }
+
+  useEffect(() => {
+    if (localStorage.name) {
+      setName(localStorage.name);
+    }
+  }, []);
 
   return (
     <Layout>
       <h1 className="mx-auto my-4 text-2xl font-bold text-center text-white">
         Melvor Idle Card Generator
       </h1>
-      <p className="text-[#bdbdbd] py-4 italic"><span className="font-bold">To find your save data</span> <div>Go to Settings &rarr; Account Settings &rarr; Import / Export Save &rarr; Export Save &rarr; Copy and paste the data in the field below.</div></p>
+      <p className="text-[#bdbdbd] py-4 italic">
+        <span className="font-bold">To find your save data</span>
+        <span className="block">
+          Go to Settings &rarr; Account Settings &rarr; Import / Export Save
+          &rarr; Export Save &rarr; Copy and paste the data in the field below.
+        </span>
+      </p>
       <form onSubmit={onSubmit} className="flex flex-col space-y-2">
         <textarea
           className="bg-[#272727] text-[#eee] w-full p-4"
@@ -39,12 +52,17 @@ const IndexPage = () => {
           cols={30}
           rows={10}
         ></textarea>
-        {error && <div className="px-4 py-2 font-semibold text-red-700 bg-red-300 border border-red-700 rounded">{error}</div>}
+        {error && (
+          <div className="px-4 py-2 font-semibold text-red-700 bg-red-300 border border-red-700 rounded">
+            {error}
+          </div>
+        )}
         <input
           className="bg-[#272727] text-white w-full p-4"
           type="text"
           placeholder="Enter character name (optional)"
           maxLength={32}
+          value={name}
           name="name"
           onChange={(e) => setName(e.target.value)}
           id="name"
@@ -57,13 +75,15 @@ const IndexPage = () => {
         </button>
       </form>
       <img src={image} alt="" className="mx-auto mt-4" />
-      {image && <a
+      {image && (
+        <a
           href={image}
           download={`${name}sMelvorIdleCard.png`}
           className="block w-full px-4 py-2 mx-auto mt-4 font-semibold text-center text-white bg-blue-600 rounded hover:shadow md:w-4/5"
         >
           Download
-        </a>}
+        </a>
+      )}
     </Layout>
   );
 };
